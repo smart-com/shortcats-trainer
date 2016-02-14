75cab87c09c4c36b3778f4d9501f28a3e4f72327
Lena <rikhnavskaja.elena@gmail.com>
2016-02-14 16:57:06 +0100

    Alt + Shift is watching

--- script.js
+++ script.js
@@ -2,9 +2,24 @@ window.onload = function () {
 
 	var playArea = document.getElementById('sandbox');
 
+	var shortcats = {
+
+		"escape" : 27,
+		"shift" : 16,
+		"alt" : 18
+	}
+
 	playArea.onkeydown = function (evt) {
 
-		//evt = evt || window.event;
-		alert(event.keyCode);
+	/** Проверяем alt + shift */
+	if(evt.keyCode === shortcats.shift)
+		playArea.value += 'shift';
+	if(evt.keyCode === shortcats.alt)
+		playArea.value += 'alt';
 	};
+
+	/** Показываем результат */
+	if(playArea.value === 'shiftalt' || playArea.value === 'altshift') {
+		playArea.value = 'You win';
+	} else playArea.value = 'Try another one...';
 };
