952c735004ce337c2fe1647d5cd277aea7b0f92f
Lena <rikhnavskaja.elena@gmail.com>
2016-02-14 20:27:14 +0100

    VERSION 0.1 IS READY

--- script.js
+++ script.js
@@ -4,22 +4,37 @@ window.onload = function () {
 
 	var shortcats = {
 
-		"escape" : 27,
-		"shift" : 16,
-		"alt" : 18
+		"escape": '27',
+		"shift": '16',
+		"alt": '18'
 	}
+
+	var counter = 0;
+	var result = '';
 
 	playArea.onkeydown = function (evt) {
 
-	/** Проверяем alt + shift */
-	if(evt.keyCode === shortcats.shift)
-		playArea.value += 'shift';
-	if(evt.keyCode === shortcats.alt)
-		playArea.value += 'alt';
-	};
+		// Считаем количество запусков скрипта
+		++counter;
 
-	/** Показываем результат */
-	if(playArea.value === 'shiftalt' || playArea.value === 'altshift') {
-		playArea.value = 'You win';
-	} else playArea.value = 'Try another one...';
+		// Записываем нажатые клавиши
+		result += evt.keyCode;
+
+		if (counter === 2) {
+
+			// Проверяем alt + shift
+			if (result === shortcats.alt + shortcats.shift && counter === 2) {
+				playArea.value = 'You win';
+				// Обнуляемся для следующей проверки
+				result = '';
+				counter = 0;
+			} else {
+				playArea.value = 'You loose...';
+
+				// Обнуляемся для следующей проверки
+				result = '';
+				counter = 0;
+			}
+		}
+	};
 };
